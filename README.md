188aad0119e4af4bd9de57dd0f02f42fbc86591e
Josh Perryman <joshperryman+github@gmail.com>
2019-06-21 23:17:36 -0500

    Menu and vertex count
    Adds menu system and first traversal

--- README.md
+++ README.md
@@ -3,7 +3,12 @@ Example graph application using Apache TinkerPop Gremlin
 
 ## Compile
 
+compile application
 ```bash
 mvn clean compile
 ```
 
+run application
+```bash
+mvn -1 clean compile exec:java -Dexec.mainClass="com.gluttonapp.App"
+```
--- src/main/java/com/gluttonapp/App.java
+++ src/main/java/com/gluttonapp/App.java
@@ -2,7 +2,10 @@ package com.gluttonapp;
 
 import org.apache.tinkerpop.gremlin.driver.Cluster;
 import org.apache.tinkerpop.gremlin.driver.remote.DriverRemoteConnection;
+import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversal;
 import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversalSource;
+
+import java.util.Scanner;
 
 import static org.apache.tinkerpop.gremlin.process.traversal.AnonymousTraversalSource.traversal;
 
@@ -14,8 +17,50 @@ public class App {
         System.out.println("Using cluster connection: " + cluster.toString());
         System.out.println("Using traversal source: " + g.toString());
 
+        displayMenu(g);
+
         cluster.close();
         System.exit(0);
+    }
+
+    private static void displayMenu(GraphTraversalSource g) {
+        int option = -1;
+        while (option != 0)
+        {
+            option = showMenu();
+            switch (option) {
+                case 1:
+                    //Get Vertex Count
+                    System.out.println("Vertex count: " + getVertexCount(g));
+                    break;
+                default:
+                    System.out.println("Sorry, please enter valid Option");
+            }
+        }
+
+        System.out.println("Exiting GluttonApp, Bye!");
+    }
+
+    private static int showMenu() {
+
+        int option = -1;
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println();
+        System.out.println("Main Menu:");
+        System.out.println("--------------");
+        System.out.println("1) Get Count of the Vertices");
+        System.out.println("0) Quit");
+        System.out.println("--------------");
+        System.out.println("Enter your choice:");
+        option = keyboard.nextInt();
+
+        return option;
+    }
+
+    private static Long getVertexCount(GraphTraversalSource g) {
+        GraphTraversal t = g.V();
+        t = t.count();
+        return (Long)t.next();
     }
 
     private static Cluster connectToDatabase() {
