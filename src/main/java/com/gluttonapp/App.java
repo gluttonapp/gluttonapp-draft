6b9b004084da8d9480e3021ea65f29f7bbfeb2ed
Josh Perryman <joshperryman+github@gmail.com>
2019-06-21 22:54:06 -0500

    Add drivers, establish connectivity
    adds TinkerPop driver and establishes connection to graph database. Cleans up some warnings during compile.

--- src/main/java/com/gluttonapp/App.java
+++ src/main/java/com/gluttonapp/App.java
@@ -1,7 +1,29 @@
 package com.gluttonapp;
 
+import org.apache.tinkerpop.gremlin.driver.Cluster;
+import org.apache.tinkerpop.gremlin.driver.remote.DriverRemoteConnection;
+import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversalSource;
+
+import static org.apache.tinkerpop.gremlin.process.traversal.AnonymousTraversalSource.traversal;
+
 public class App {
     public static void main(String[] args) {
+        Cluster cluster = connectToDatabase();
+        GraphTraversalSource g = getGraphTraversalSource(cluster);
+
+        cluster.close();
         System.exit(0);
+    }
+
+    private static Cluster connectToDatabase() {
+        Cluster.Builder builder = Cluster.build();
+        builder.addContactPoint("localhost");
+        builder.port(8182);
+
+        return builder.create();
+    }
+
+    public static GraphTraversalSource getGraphTraversalSource(Cluster cluster) {
+        return traversal().withRemote(DriverRemoteConnection.using(cluster));
     }
 }
